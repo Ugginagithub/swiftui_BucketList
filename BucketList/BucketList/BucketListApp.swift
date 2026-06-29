d7325f1dbe19db7c64ebcef5cddb1ed70735a9c0
UGGINA TARUN PAVAN KUMAR <98165814+Ugginagithub@users.noreply.github.com>
2026-06-29 12:18:32 +0530

    BucketList app used to understand the CoreImage
    BucketList app used to understand the CoreImage

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Tarun on 29/06/26.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
